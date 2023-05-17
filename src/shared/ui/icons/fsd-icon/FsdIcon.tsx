d5a3de5284277ce880bc2e73bced02378bcc134a
Yuri <118747540+sldk-yuri@users.noreply.github.com>
2023-05-18 00:00:29 +0300

    feat(shared/ui/icons): add custom icons

---
+++ src/shared/ui/icons/fsd-icon/FsdIcon.tsx
@@ -0,0 +1,457 @@
+import SvgIcon, {
+  SvgIconProps as MuiSvgIconProps,
+} from '@mui/material/SvgIcon';
+
+type SvgIconProps = Omit<MuiSvgIconProps, 'viewBox'>;
+
+export function FsdIcon(props: SvgIconProps) {
+  return (
+    <SvgIcon {...props} viewBox="0 0 365 114">
+      <g clipPath="url(#clip0_503_693)">
+        <g filter="url(#filter0_d_503_693)">
+          <path d="M8 4H108V13H8V4Z" fill="url(#paint0_linear_503_693)" />
+          <path d="M8 17H108V26H8V17Z" fill="url(#paint1_linear_503_693)" />
+          <path d="M8 43H108V52H8V43Z" fill="url(#paint2_linear_503_693)" />
+          <path d="M8 56H108V65H8V56Z" fill="url(#paint3_linear_503_693)" />
+          <path d="M8 69H56V78H8V69Z" fill="url(#paint4_linear_503_693)" />
+          <path d="M8 30H56V39H8V30Z" fill="url(#paint5_linear_503_693)" />
+          <path d="M8 82H56V91H8V82Z" fill="url(#paint6_linear_503_693)" />
+          <path d="M8 95H56V104H8V95Z" fill="url(#paint7_linear_503_693)" />
+        </g>
+        <g filter="url(#filter1_d_503_693)">
+          <path d="M258 4H319V13H258V4Z" fill="url(#paint8_linear_503_693)" />
+          <path d="M258 17H345V26H258V17Z" fill="url(#paint9_linear_503_693)" />
+          <path
+            d="M258 43H358V52H258V43Z"
+            fill="url(#paint10_linear_503_693)"
+          />
+          <path
+            d="M258 56H358V65H258V56Z"
+            fill="url(#paint11_linear_503_693)"
+          />
+          <path
+            d="M258 69H358V78H258V69Z"
+            fill="url(#paint12_linear_503_693)"
+          />
+          <path
+            d="M258 30H358V39H258V30Z"
+            fill="url(#paint13_linear_503_693)"
+          />
+          <path
+            d="M258 82H345V91H258V82Z"
+            fill="url(#paint14_linear_503_693)"
+          />
+          <path
+            d="M258 95H319V104H258V95Z"
+            fill="url(#paint15_linear_503_693)"
+          />
+        </g>
+        <g filter="url(#filter2_d_503_693)">
+          <path d="M159 4H233V13H159V4Z" fill="url(#paint16_linear_503_693)" />
+          <path
+            d="M133 17H233V26H133V17Z"
+            fill="url(#paint17_linear_503_693)"
+          />
+          <path
+            d="M133 30H207V39H133V30Z"
+            fill="url(#paint18_linear_503_693)"
+          />
+          <path
+            d="M146 43H207V52H146V43Z"
+            fill="url(#paint19_linear_503_693)"
+          />
+          <path
+            d="M159 56H220V65H159V56Z"
+            fill="url(#paint20_linear_503_693)"
+          />
+          <path
+            d="M159 69H233V78H159V69Z"
+            fill="url(#paint21_linear_503_693)"
+          />
+          <path
+            d="M133 82H233V91H133V82Z"
+            fill="url(#paint22_linear_503_693)"
+          />
+          <path
+            d="M133 95H207V104H133V95Z"
+            fill="url(#paint23_linear_503_693)"
+          />
+        </g>
+      </g>
+      <defs>
+        <filter
+          id="filter0_d_503_693"
+          x="0"
+          y="-2"
+          width="116"
+          height="116"
+          filterUnits="userSpaceOnUse"
+          colorInterpolationFilters="sRGB"
+        >
+          <feFlood floodOpacity="0" result="BackgroundImageFix" />
+          <feColorMatrix
+            in="SourceAlpha"
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+            result="hardAlpha"
+          />
+          <feOffset dy="2" />
+          <feGaussianBlur stdDeviation="4" />
+          <feComposite in2="hardAlpha" operator="out" />
+          <feColorMatrix
+            type="matrix"
+            values="0 0 0 0 0.121569 0 0 0 0 0.517647 0 0 0 0 0.980392 0 0 0 1 0"
+          />
+          <feBlend
+            mode="normal"
+            in2="BackgroundImageFix"
+            result="effect1_dropShadow_503_693"
+          />
+          <feBlend
+            mode="normal"
+            in="SourceGraphic"
+            in2="effect1_dropShadow_503_693"
+            result="shape"
+          />
+        </filter>
+        <filter
+          id="filter1_d_503_693"
+          x="250"
+          y="-2"
+          width="116"
+          height="116"
+          filterUnits="userSpaceOnUse"
+          colorInterpolationFilters="sRGB"
+        >
+          <feFlood floodOpacity="0" result="BackgroundImageFix" />
+          <feColorMatrix
+            in="SourceAlpha"
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+            result="hardAlpha"
+          />
+          <feOffset dy="2" />
+          <feGaussianBlur stdDeviation="4" />
+          <feComposite in2="hardAlpha" operator="out" />
+          <feColorMatrix
+            type="matrix"
+            values="0 0 0 0 0.121569 0 0 0 0 0.517647 0 0 0 0 0.980392 0 0 0 1 0"
+          />
+          <feBlend
+            mode="normal"
+            in2="BackgroundImageFix"
+            result="effect1_dropShadow_503_693"
+          />
+          <feBlend
+            mode="normal"
+            in="SourceGraphic"
+            in2="effect1_dropShadow_503_693"
+            result="shape"
+          />
+        </filter>
+        <filter
+          id="filter2_d_503_693"
+          x="125"
+          y="-2"
+          width="116"
+          height="116"
+          filterUnits="userSpaceOnUse"
+          colorInterpolationFilters="sRGB"
+        >
+          <feFlood floodOpacity="0" result="BackgroundImageFix" />
+          <feColorMatrix
+            in="SourceAlpha"
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+            result="hardAlpha"
+          />
+          <feOffset dy="2" />
+          <feGaussianBlur stdDeviation="4" />
+          <feComposite in2="hardAlpha" operator="out" />
+          <feColorMatrix
+            type="matrix"
+            values="0 0 0 0 0.122396 0 0 0 0 0.51651 0 0 0 0 0.979167 0 0 0 1 0"
+          />
+          <feBlend
+            mode="normal"
+            in2="BackgroundImageFix"
+            result="effect1_dropShadow_503_693"
+          />
+          <feBlend
+            mode="normal"
+            in="SourceGraphic"
+            in2="effect1_dropShadow_503_693"
+            result="shape"
+          />
+        </filter>
+        <linearGradient
+          id="paint0_linear_503_693"
+          x1="8"
+          y1="4"
+          x2="108"
+          y2="104"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#29BEDC" />
+          <stop offset="1" stopColor="#517AED" />
+        </linearGradient>
+        <linearGradient
+          id="paint1_linear_503_693"
+          x1="8"
+          y1="4"
+          x2="108"
+          y2="104"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#29BEDC" />
+          <stop offset="1" stopColor="#517AED" />
+        </linearGradient>
+        <linearGradient
+          id="paint2_linear_503_693"
+          x1="8"
+          y1="4"
+          x2="108"
+          y2="104"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#29BEDC" />
+          <stop offset="1" stopColor="#517AED" />
+        </linearGradient>
+        <linearGradient
+          id="paint3_linear_503_693"
+          x1="8"
+          y1="4"
+          x2="108"
+          y2="104"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#29BEDC" />
+          <stop offset="1" stopColor="#517AED" />
+        </linearGradient>
+        <linearGradient
+          id="paint4_linear_503_693"
+          x1="8"
+          y1="4"
+          x2="108"
+          y2="104"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#29BEDC" />
+          <stop offset="1" stopColor="#517AED" />
+        </linearGradient>
+        <linearGradient
+          id="paint5_linear_503_693"
+          x1="8"
+          y1="4"
+          x2="108"
+          y2="104"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#29BEDC" />
+          <stop offset="1" stopColor="#517AED" />
+        </linearGradient>
+        <linearGradient
+          id="paint6_linear_503_693"
+          x1="8"
+          y1="4"
+          x2="108"
+          y2="104"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#29BEDC" />
+          <stop offset="1" stopColor="#517AED" />
+        </linearGradient>
+        <linearGradient
+          id="paint7_linear_503_693"
+          x1="8"
+          y1="4"
+          x2="108"
+          y2="104"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#29BEDC" />
+          <stop offset="1" stopColor="#517AED" />
+        </linearGradient>
+        <linearGradient
+          id="paint8_linear_503_693"
+          x1="258"
+          y1="4"
+          x2="358"
+          y2="104"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#29BEDC" />
+          <stop offset="1" stopColor="#517AED" />
+        </linearGradient>
+        <linearGradient
+          id="paint9_linear_503_693"
+          x1="258"
+          y1="4"
+          x2="358"
+          y2="104"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#29BEDC" />
+          <stop offset="1" stopColor="#517AED" />
+        </linearGradient>
+        <linearGradient
+          id="paint10_linear_503_693"
+          x1="258"
+          y1="4"
+          x2="358"
+          y2="104"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#29BEDC" />
+          <stop offset="1" stopColor="#517AED" />
+        </linearGradient>
+        <linearGradient
+          id="paint11_linear_503_693"
+          x1="258"
+          y1="4"
+          x2="358"
+          y2="104"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#29BEDC" />
+          <stop offset="1" stopColor="#517AED" />
+        </linearGradient>
+        <linearGradient
+          id="paint12_linear_503_693"
+          x1="258"
+          y1="4"
+          x2="358"
+          y2="104"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#29BEDC" />
+          <stop offset="1" stopColor="#517AED" />
+        </linearGradient>
+        <linearGradient
+          id="paint13_linear_503_693"
+          x1="258"
+          y1="4"
+          x2="358"
+          y2="104"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#29BEDC" />
+          <stop offset="1" stopColor="#517AED" />
+        </linearGradient>
+        <linearGradient
+          id="paint14_linear_503_693"
+          x1="258"
+          y1="4"
+          x2="358"
+          y2="104"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#29BEDC" />
+          <stop offset="1" stopColor="#517AED" />
+        </linearGradient>
+        <linearGradient
+          id="paint15_linear_503_693"
+          x1="258"
+          y1="4"
+          x2="358"
+          y2="104"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#29BEDC" />
+          <stop offset="1" stopColor="#517AED" />
+        </linearGradient>
+        <linearGradient
+          id="paint16_linear_503_693"
+          x1="133"
+          y1="4"
+          x2="233"
+          y2="104"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#29BEDC" />
+          <stop offset="1" stopColor="#517AED" />
+        </linearGradient>
+        <linearGradient
+          id="paint17_linear_503_693"
+          x1="133"
+          y1="4"
+          x2="233"
+          y2="104"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#29BEDC" />
+          <stop offset="1" stopColor="#517AED" />
+        </linearGradient>
+        <linearGradient
+          id="paint18_linear_503_693"
+          x1="133"
+          y1="4"
+          x2="233"
+          y2="104"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#29BEDC" />
+          <stop offset="1" stopColor="#517AED" />
+        </linearGradient>
+        <linearGradient
+          id="paint19_linear_503_693"
+          x1="133"
+          y1="4"
+          x2="233"
+          y2="104"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#29BEDC" />
+          <stop offset="1" stopColor="#517AED" />
+        </linearGradient>
+        <linearGradient
+          id="paint20_linear_503_693"
+          x1="133"
+          y1="4"
+          x2="233"
+          y2="104"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#29BEDC" />
+          <stop offset="1" stopColor="#517AED" />
+        </linearGradient>
+        <linearGradient
+          id="paint21_linear_503_693"
+          x1="133"
+          y1="4"
+          x2="233"
+          y2="104"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#29BEDC" />
+          <stop offset="1" stopColor="#517AED" />
+        </linearGradient>
+        <linearGradient
+          id="paint22_linear_503_693"
+          x1="133"
+          y1="4"
+          x2="233"
+          y2="104"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#29BEDC" />
+          <stop offset="1" stopColor="#517AED" />
+        </linearGradient>
+        <linearGradient
+          id="paint23_linear_503_693"
+          x1="133"
+          y1="4"
+          x2="233"
+          y2="104"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#29BEDC" />
+          <stop offset="1" stopColor="#517AED" />
+        </linearGradient>
+        <clipPath id="clip0_503_693">
+          <rect width="365" height="114" fill="white" />
+        </clipPath>
+      </defs>
+    </SvgIcon>
+  );
+}
---
+++ src/shared/ui/icons/index.ts
@@ -0,0 +1,4 @@
+export { FsdIcon } from './fsd-icon';
+export { MuiIcon } from './mui-icon';
+export { ReactIcon } from './react-icon';
+export { ViteIcon } from './vite-icon';
