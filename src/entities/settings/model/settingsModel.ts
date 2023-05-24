f350cb4c278952cd45824241613ce2976d0ef91b
Yuri <118747540+sldk-yuri@users.noreply.github.com>
2023-05-24 16:13:11 +0300

    feat(entities/settings): add settings model

---
+++ src/entities/settings/model/settingsModel.ts
@@ -0,0 +1,35 @@
+import { PaletteMode } from '@mui/material';
+import { StateCreator, createStore } from 'zustand';
+import { devtools, persist } from 'zustand/middleware';
+
+export type ThemeMode = PaletteMode;
+
+export type SettingsState = {
+  themeMode: ThemeMode;
+  setThemeMode: (themeMode: ThemeMode) => void;
+};
+
+// TODO: add system preference theme mode checker
+const createSettingsSlice: StateCreator<
+  SettingsState,
+  [['zustand/devtools', never], ['zustand/persist', unknown]],
+  [],
+  SettingsState
+> = (set) => ({
+  themeMode: 'dark',
+
+  setThemeMode: (themeMode: ThemeMode) =>
+    set({ themeMode }, false, 'settings/setThemeMode'),
+});
+
+export const settingsStore = createStore<SettingsState>()(
+  persist(
+    devtools(
+      (...a) => ({
+        ...createSettingsSlice(...a),
+      }),
+      { name: 'Settings Store' },
+    ),
+    { name: 'settings' },
+  ),
+);
